b7abb5d801b9946d1897929a1ef1ace2348410b1
remondawagwwh <remondawageh88@gmail.com>
2025-05-20 02:55:19 +0300

    add from .py

--- form_model/products/views.py
+++ form_model/products/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Product, Category
+from django.http import HttpResponse
+from .forms import BookForm, BookFormModel
+import os
+from django.conf import settings
+from django.views import View
 
 # Create your views here.
