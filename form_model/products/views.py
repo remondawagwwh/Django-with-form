3756bcc2af2e071bcf51504a1297ccbacb1c5e9a
remondawagwwh <remondawageh88@gmail.com>
2025-05-20 03:20:10 +0300

    form.py done

--- form_model/products/views.py
+++ form_model/products/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.shortcuts import render, redirect
 from .models import Product, Category
 from django.http import HttpResponse
-from .forms import BookForm, BookFormModel
+from .forms import ProductForm, ProductFormModel
 import os
 from django.conf import settings
 from django.views import View
